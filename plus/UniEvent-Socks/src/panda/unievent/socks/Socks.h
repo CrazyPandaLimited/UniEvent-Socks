7c86b0803b6b61bb16c83d4c1dcf9961d0b20675
syber <devnull@localhost>
2018-11-02 19:19:03 +0300

    UniEvent::Socks: misc

--- plus/UniEvent-Socks/src/panda/unievent/socks/Socks.h
+++ plus/UniEvent-Socks/src/panda/unievent/socks/Socks.h
@@ -9,22 +9,18 @@ namespace panda { namespace unievent { namespace socks {
 struct Socks : virtual Refcnt {
     using URI = panda::uri::URI;
 
+    static constexpr int MAX_LOGPASS_LENGTH = 255;
+
     Socks (const string& host, uint16_t port, const string& login = "", const string& passw = "", bool socks_resolve = true)
-            : host(host), port(port), login(login), passw(passw), socks_resolve(socks_resolve) {}
+            : host(host), port(port), login(login), passw(passw), socks_resolve(socks_resolve)
+    {
+        if (login.length() > MAX_LOGPASS_LENGTH) throw Error("Bad login length");
+        if (passw.length() > MAX_LOGPASS_LENGTH) throw Error("Bad password length");
+    }
 
     Socks (const string& uri, bool socks_resolve = true) : Socks(URI::socks(uri), socks_resolve) {}
 
-    Socks (const URI::socks& uri, bool socks_resolve = true) : socks_resolve(socks_resolve) {
-        if (uri.host()) {
-            host  = uri.host();
-            port  = uri.port();
-            login = uri.user();
-            passw = uri.password();
-        }
-
-        if (login.length() > 0xFF) throw Error("Bad login length");
-        if (passw.length() > 0xFF) throw Error("Bad password length");
-    }
+    Socks (const URI::socks& uri, bool socks_resolve = true) : Socks(uri.host(), uri.port(), uri.user(), uri.password(), socks_resolve) {}
 
     bool configured () const { return !host.empty(); }
     bool loginpassw () const { return !login.empty(); }
