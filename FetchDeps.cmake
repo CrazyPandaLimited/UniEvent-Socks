0244cec00fe473ac971bb1561137504967f44d80
g.smorkalov <g.smorkalov@crazypanda.ru>
2023-03-22 14:26:55 +0300

    UE-Socks: CMake fix

--- FetchDeps.cmake
+++ FetchDeps.cmake
@@ -11,7 +11,7 @@ set(deps unievent panda-uri)
 set(UNIEVENT_FETCH_DEPS ON)
 set(PANDA_URI_FETCH_DEPS ON)
 
-if (${UNIEVENT_TESTS})
+if (${UNIEVENT_SOCKS_TESTS})
     list(APPEND deps Catch2)
 endif()
 
